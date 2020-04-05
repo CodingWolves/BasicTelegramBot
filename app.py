331ff457527c366781beec72bb21ddc2cb3d1715
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-04-05 20:56:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def respond():
        """
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
+    elif text == "/end":
+        bot.sendMessage(chat_id=chat_id, text="Bye", reply_to_message_id=msg_id)
     else:
         try:
             # clear the message we got from any non alphabets
