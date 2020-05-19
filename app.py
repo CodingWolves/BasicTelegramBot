0cd01e2b3bb501c51b683d1d2cce6d2f9abc571d
idozahavy <idozahavy@users.noreply.github.com>
2020-05-19 22:39:39 +0300

    hi response

--- app.py
+++ app.py
@@ -31,6 +31,9 @@ def respond():
        """
         # send the welcoming message
         bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
+    elif text == "hi":
+        respond_text = "hello to you too :)"
+        bot.sendMessage(chat_id=chat_id, text=respond_text, reply_to_message_id=msg_id)
     elif text == "bye":
         bot.send_animation(chat_id=chat_id, animation="animations/bye_bye.mp4", reply_to_message_id=msg_id)
     else:
