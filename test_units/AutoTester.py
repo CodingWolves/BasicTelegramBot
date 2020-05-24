c70855b95abec71cd3115ccd4c4fc192bd651bf3
idozahavy <idozahavy@users.noreply.github.com>
2020-05-24 21:07:19 +0300

    fixed order and prints elasped time for every tester

--- test_units/AutoTester.py
+++ test_units/AutoTester.py
@@ -1,4 +1,5 @@
-from test_units.messageTester import MessageClass, getCustomizedSequence, getSequenceFromActDict , getRandomSequenceFromActDict
+from test_units.messageTester import MessageClass, getCustomizedSequence, getSequenceFromActDict, \
+    getRandomSequenceFromActDict
 from telebot.Chat import Chat
 from telebot.Act import InitializeActs
 from telebot.credentials import URL
@@ -17,16 +18,19 @@ def getRandText():
 
 
 def StartTester():
-    seq_total_time = datetime.timedelta()
-    msg_total_time = datetime.timedelta()
+    smart_seq_total_time = datetime.timedelta()
+    garbage_total_time = datetime.timedelta()
+    custom_seq_total_time = datetime.timedelta()
 
     print(random.random())
     global actions
     InitializeActs()
+
     chats_count = 1000
     garbage_count = 100
-    sequences_count = 10
-    customized_sequences_count = 10
+    customized_sequences_count = 100
+    smart_sequences_count = 100
+
     bot = BotClass()
     first_messages = [MessageClass(message_id=randint(10000, 99999), text=getRandText(),
                                    chat_id=randint(10000, 99999), chat_first_name='ido', chat_last_name='zany')
@@ -37,30 +41,40 @@ def StartTester():
 
         msg_time = datetime.datetime.now()
         GarbageSender(bot, chat, garbage_count)
-        msg_total_time += datetime.datetime.now() - msg_time
+        garbage_total_time += datetime.datetime.now() - msg_time
 
-        for _ in range(6):
-            chat.GotMessage(bot=bot, message=MessageClass(message_id=randint(10000, 99999), text='abiding',
-                                                          chat_id=chat.id, chat_first_name='ido', chat_last_name='zany'))
+        GarbageSender(bot, chat, 10, text="abiding")  # resets the chat follow_up because of previous garbage
 
+        custom_seq_time = datetime.datetime.now()
         for _ in range(customized_sequences_count):
             actions.clear()
             sequence = getCustomizedSequence(randint(0, 10000))
             print('new customized sequence - {}'.format(sequence))
             SequenceSender(bot, chat, sequence)
+        custom_seq_total_time += datetime.datetime.now() - custom_seq_time
 
         seq_time = datetime.datetime.now()
-        RandomSequenceSender(bot, chat, sequences_count)
-        seq_total_time += datetime.datetime.now() - seq_time
+        for _ in range(smart_sequences_count):
+            sequence = getRandomSequenceFromActDict(chat.data)
+            SequenceSender(bot, chat, sequence)
+        smart_seq_total_time += datetime.datetime.now() - seq_time
 
-    print('msg time {}'.format(msg_total_time))
-    print('seq time {}'.format(seq_total_time))
+    print('chats count {}'.format(chats_count))
+    print('garbage msg count {} , time {}'.format(garbage_count, garbage_total_time))
+    print('custom seq count {} , time {}'.format(customized_sequences_count, custom_seq_total_time))
+    print('smart seq count {} , time {}'.format(smart_sequences_count, smart_seq_total_time))
 
 
-def GarbageSender(bot, chat, count: int):
-    messages = [MessageClass(message_id=randint(10000, 99999), text=getRandText(),
-                             chat_id=chat.id, chat_first_name='ido', chat_last_name='zany')
-                for i in range(count)]
+def GarbageSender(bot, chat, count: int, text=None):
+    messages = None
+    if text:
+        messages = [MessageClass(message_id=randint(10000, 99999), text=text,
+                                 chat_id=chat.id, chat_first_name='ido', chat_last_name='zany')
+                    for i in range(count)]
+    else:
+        messages = [MessageClass(message_id=randint(10000, 99999), text=getRandText(),
+                                 chat_id=chat.id, chat_first_name='ido', chat_last_name='zany')
+                    for i in range(count)]
     for msg in messages:
         actions.clear()
         chat.GotMessage(bot=bot, message=msg)
@@ -98,12 +112,6 @@ def CheckResponseActions(sequence):
         for i in range(len(sequence['responses']), len(actions), 1):
             print('action by server - {}'.format(actions[i]))
         raise Exception("more responses than expected".format())
-
-
-def RandomSequenceSender(bot, chat, count: int):
-    for _ in range(count):
-        sequence = getRandomSequenceFromActDict(chat.data)
-        SequenceSender(bot, chat, sequence)
 
 
 class BotClass:
