d57d812e1ba8c19a3ca134661d1ef57633821c70
idozahavy <idozahavy@users.noreply.github.com>
2020-05-24 20:52:24 +0300

    3 diffrent testers
    [ ActDict retrivel , customized texts and responses , garbage ] Testers

--- test_units/AutoTester.py
+++ test_units/AutoTester.py
@@ -1,6 +1,7 @@
-from test_units.messageTester import MessageClass, getMessageSequence
+from test_units.messageTester import MessageClass, getCustomizedSequence, getSequenceFromActDict , getRandomSequenceFromActDict
 from telebot.Chat import Chat
 from telebot.Act import InitializeActs
+from telebot.credentials import URL
 from random import randint
 import random
 import datetime
@@ -19,13 +20,13 @@ def StartTester():
     seq_total_time = datetime.timedelta()
     msg_total_time = datetime.timedelta()
 
-
     print(random.random())
     global actions
     InitializeActs()
     chats_count = 1000
-    messages_count = 10
+    garbage_count = 100
     sequences_count = 10
+    customized_sequences_count = 10
     bot = BotClass()
     first_messages = [MessageClass(message_id=randint(10000, 99999), text=getRandText(),
                                    chat_id=randint(10000, 99999), chat_first_name='ido', chat_last_name='zany')
@@ -33,47 +34,76 @@ def StartTester():
     chats = [Chat(msg) for msg in first_messages]
     for chat in chats:
         assert issubclass(type(chat), Chat)
-        messages = [MessageClass(message_id=randint(10000, 99999), text=getRandText(),
-                                 chat_id=chat.id, chat_first_name='ido', chat_last_name='zany')
-                    for i in range(messages_count)]
 
         msg_time = datetime.datetime.now()
-        for msg in messages:
-            actions.clear()
-            chat.GotMessage(bot=bot, message=msg)
-            for action in actions:
-                print('action by server - {}'.format(action))
-            pass
+        GarbageSender(bot, chat, garbage_count)
         msg_total_time += datetime.datetime.now() - msg_time
 
         for _ in range(6):
             chat.GotMessage(bot=bot, message=MessageClass(message_id=randint(10000, 99999), text='abiding',
                                                           chat_id=chat.id, chat_first_name='ido', chat_last_name='zany'))
-        seq_time = datetime.datetime.now()
-        message_sequences = [getMessageSequence(randint(0, 999999)) for _ in range(sequences_count)]
-        for message_sequence in message_sequences:
+
+        for _ in range(customized_sequences_count):
             actions.clear()
-            print('new message sequence started {}'.format(message_sequence))
-            for text in message_sequence['texts']:
-                chat.GotMessage(bot=bot, message=MessageClass(message_id=randint(10000, 99999), text=text,
-                                                              chat_id=chat.id, chat_first_name='ido', chat_last_name='zany'))
-            for response in message_sequence['responses']:
-                if len(actions) > 0:
-                    if actions[0]['text'] == response:
-                        print('action by server - {}'.format(actions.pop(0)))
-                    else:
-                        raise Exception("response was not correct '{}' , needed '{}'".format(actions[0], response))
-                else:
-                    raise Exception('fewer actions by server than expected'.format())
-            if len(actions) > 0:
-                print('Unexpected Actions')
-                for action in actions:
-                    print('action by server - {}'.format(action))
-                raise Exception("more responses than expected, ".format(actions[0], response))
+            sequence = getCustomizedSequence(randint(0, 10000))
+            print('new customized sequence - {}'.format(sequence))
+            SequenceSender(bot, chat, sequence)
+
+        seq_time = datetime.datetime.now()
+        RandomSequenceSender(bot, chat, sequences_count)
         seq_total_time += datetime.datetime.now() - seq_time
 
     print('msg time {}'.format(msg_total_time))
     print('seq time {}'.format(seq_total_time))
+
+
+def GarbageSender(bot, chat, count: int):
+    messages = [MessageClass(message_id=randint(10000, 99999), text=getRandText(),
+                             chat_id=chat.id, chat_first_name='ido', chat_last_name='zany')
+                for i in range(count)]
+    for msg in messages:
+        actions.clear()
+        chat.GotMessage(bot=bot, message=msg)
+        for action in actions:
+            print('action by server - {}'.format(action))
+
+
+def SequenceSender(bot, chat, sequence):
+    global actions
+    print('new random sequence formed - {}'.format(sequence))
+    actions.clear()
+    for text in sequence['texts']:
+        print(chat.data.user.first_name)
+        chat.GotMessage(bot=bot, message=MessageClass(message_id=randint(10000, 99999), text=text,
+                                                      chat_id=chat.id, chat_first_name='ido',
+                                                      chat_last_name='zany'))
+    CheckResponseActions(sequence)
+
+
+def CheckResponseActions(sequence):
+    for i in range(len(sequence['responses'])):
+        response = sequence['responses'][i]
+        if len(actions) > i:
+            if 'text' in actions[i] and actions[i]['text'] == response:
+                print('checked action for server - {}'.format(actions[i]))
+            elif 'animation' in actions[i] and actions[i]['animation'] == response:
+                print('checked action for server - {}'.format(actions[i]))
+            else:
+                print(actions)
+                raise Exception("response was not correct '{}' , needed '{}'".format(actions[i], response))
+        else:
+            raise Exception('fewer actions by server than expected'.format())
+    if len(actions) > len(sequence['responses']):
+        print('Unexpected Actions ---')
+        for i in range(len(sequence['responses']), len(actions), 1):
+            print('action by server - {}'.format(actions[i]))
+        raise Exception("more responses than expected".format())
+
+
+def RandomSequenceSender(bot, chat, count: int):
+    for _ in range(count):
+        sequence = getRandomSequenceFromActDict(chat.data)
+        SequenceSender(bot, chat, sequence)
 
 
 class BotClass:
--- test_units/messageTester.py
+++ test_units/messageTester.py
@@ -1,8 +1,12 @@
 from telebot.Generic import Object
+from telebot.ActDict import ActionsDictionary, ActType
+from random import randint
+from telebot.credentials import URL
 
 
 class MessageClass(Object):
     def __init__(self, message_id: int, text: str, chat_id: int, chat_first_name: str, chat_last_name: str):
+        super(MessageClass, self).__init__()
         self.message_id = message_id
         self.text = text
         self.chat = Object
@@ -11,9 +15,72 @@ class MessageClass(Object):
         self.chat.last_name = chat_last_name
 
 
-def getMessageSequence(index: int):
+def getCustomizedSequence(index: int):
     return message_sequences[index % len(message_sequences)]
     pass
+
+
+def getRandomSequenceFromActDict(server_data=Object()):
+    index = randint(0, len(ActionsDictionary) - 1)
+    while len(ActionsDictionary[index]['triggers']) == 0:
+        index = randint(0, len(ActionsDictionary) - 1)
+    return getSequenceFromActDict(ActionsDictionary[index]['id'], server_data=server_data)
+
+
+def getSequenceFromActDict(act_id: int, server_data=Object(), is_follow_up=False):
+    action_dict = None
+    for act in ActionsDictionary:
+        if act['id'] == act_id:
+            action_dict = act
+            break
+    if not action_dict:
+        raise Exception('cant find id - {}'.format(act_id))
+    print('act_id - {} , is_follow_up - {} , data - {}'.format(act_id, is_follow_up, server_data))
+
+    user_messages = []
+    server_messages = []
+    if not is_follow_up:
+        if action_dict['type'] == ActType.Text or action_dict['type'] == ActType.Animation:
+            user_msg = None
+            if len(action_dict['triggers']) > 0:
+                user_msg = action_dict['triggers'][randint(0, len(action_dict['triggers'])-1)]
+                user_messages.append(user_msg)
+            server_msg = action_dict['data'].format(text_message=user_msg, data=server_data, URL=URL)
+            server_messages.append(server_msg)
+        elif action_dict['type'] == ActType.SaveCommand:
+            data_name = action_dict['save_to_data_name']
+            server_data[data_name] = action_dict['data'].format(data=server_data)
+        else:
+            raise Exception('101')
+    else:
+        if action_dict['type'] == ActType.SaveCommand:
+            user_msg = action_dict['data'].format(text_message=getRandomInput(), data=server_data)
+            user_messages.append(user_msg)
+            data_name = action_dict['save_to_data_name']
+            server_data[data_name] = user_msg.format(text_message=user_msg, data=server_data)
+        else:
+            raise Exception('102')
+
+    if 'next_act_id' in action_dict:
+        next_act_message = getSequenceFromActDict(action_dict['next_act_id'], server_data=server_data)
+        for user_msg in next_act_message['texts']:
+            user_messages.append(user_msg)
+        for server_msg in next_act_message['responses']:
+            server_messages.append(server_msg)
+
+    elif 'follow_up_act_id' in action_dict:
+        follow_up_act_message = getSequenceFromActDict(action_dict['follow_up_act_id'],
+                                                       server_data=server_data, is_follow_up=True)
+        for user_msg in follow_up_act_message['texts']:
+            user_messages.append(user_msg)
+        for server_msg in follow_up_act_message['responses']:
+            server_messages.append(server_msg)
+
+    return {'texts': user_messages, 'responses': server_messages}
+
+
+def getRandomInput():
+    return NamesTemplates[randint(0, len(NamesTemplates)-1)]
 
 
 NamesTemplates = ['ido', 'nadav', 'gal', 'ben', 'alon', 'adi', 'omer', 'inbal', 'shir', 'dor']
